4d07d54004ad45b86ebf21b841c95ab871316b69
jajan20 <jamie.jansen2@hva.nl>
2019-08-12 16:04:16 +0200

    added math.floor for minutes.

--- script.js
+++ script.js
@@ -10,8 +10,45 @@ let endtimeInput = document.querySelector('#end-time')
 submitBtn.addEventListener('click', (event) => {
   event.preventDefault()
 
+  /*----------------------*/
+  /* Calculate StartTime */
+  /*--------------------*/
   let startTimeValue = startTimeInput.value.split(':')
+  if (startTimeValue[1] < 15 && startTimeValue[1] > 0) {
+    startTimeValue[1] = 00
+  } 
+
+  if (startTimeValue[1] < 30 && startTimeValue[1] > 15) {
+    startTimeValue[1] = 15
+  } 
+
+  if (startTimeValue[1] < 45 && startTimeValue[1] > 30) {
+    startTimeValue[1] = 30
+  }
+
+  if (startTimeValue[1] < 60 && startTimeValue[1] > 46) {
+    startTimeValue[1] = 45
+  } 
+
+  /*--------------------*/
+  /* Calculate EndTime */
+  /*------------------*/
   let endTimeValue = endtimeInput.value.split(':')
+  if (endTimeValue[1] < 15 && endTimeValue[1] > 0) {
+    endTimeValue[1] = 00
+  } 
+
+  if (endTimeValue[1] < 30 && endTimeValue[1] > 15) {
+    endTimeValue[1] = 15
+  } 
+
+  if (endTimeValue[1] < 45 && endTimeValue[1] > 30) {
+    endTimeValue[1] = 30
+  }
+
+  if (endTimeValue[1] < 60 && endTimeValue[1] > 46) {
+    endTimeValue[1] = 45
+  } 
 
   // if (firstBreak.checked) {
   // 	object.firstBreak = parseInt(firstBreak.value, 10)  
@@ -33,7 +70,7 @@ submitBtn.addEventListener('click', (event) => {
   
   TIME_ENTRY.startTime = parseInt(startTimeValue[0], 10) * 60 + parseInt(startTimeValue[1], 10)
   TIME_ENTRY.endTime = parseInt(endTimeValue[0], 10) * 60 + parseInt(endTimeValue[1], 10)
-  TIME_ENTRY.workedHours = (TIME_ENTRY.endTime - TIME_ENTRY.startTime) / 60
+  TIME_ENTRY.workedHours = Math.round((TIME_ENTRY.endTime - TIME_ENTRY.startTime) / 60 * 100) / 100
 
   console.log(TIME_ENTRY)
 
