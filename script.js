821790b0d234bbfcc0c47f5461e40fb5a4756ecd
jajan20 <jamie.jansen2@hva.nl>
2019-08-12 15:30:40 +0200

    fixed basic math for working with time

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-let object = {}
+let TIME_ENTRY = {}
 
 let submitBtn = document.querySelector('#submit')
-let startTime = document.querySelector('#start-time')
-let endTime = document.querySelector('#end-time')
-let firstBreak = document.querySelector('#first-break')
-let secondBreak = document.querySelector('#second-break')
-let thirdBreak = document.querySelector('#third-break')
+let startTimeInput = document.querySelector('#start-time')
+let endtimeInput = document.querySelector('#end-time')
+// let firstBreak = document.querySelector('#first-break')
+// let secondBreak = document.querySelector('#second-break')
+// let thirdBreak = document.querySelector('#third-break')
 
 submitBtn.addEventListener('click', (event) => {
-	event.preventDefault()
-	object.startTime = parseInt(startTime.value, 10) * 60
-  object.endTime = parseInt(endTime.value, 10) * 60
+  event.preventDefault()
+
+  let startTimeValue = startTimeInput.value.split(':')
+  let endTimeValue = endtimeInput.value.split(':')
+
+  // if (firstBreak.checked) {
+  // 	object.firstBreak = parseInt(firstBreak.value, 10)  
+  // } else {
+  // 	  object.firstBreak = 0
+  // }
+
+  // if (secondBreak.checked) {
+  // 	object.secondBreak = parseInt(secondBreak.value, 10)  
+  // } else {
+  // 	  object.secondBreak = 0
+  // }
+
+  // if (thirdBreak.checked) {
+  // 	object.thirdBreak = parseInt(thirdBreak.value, 10)  
+  // } else {
+  // 	  object.thirdBreak = 0
+  // }
   
-  if (firstBreak.checked) {
-		object.firstBreak = parseInt(firstBreak.value, 10)  
-  } else {
-  	  object.firstBreak = 0
-  }
-  
-  if (secondBreak.checked) {
-		object.secondBreak = parseInt(secondBreak.value, 10)  
-  } else {
-  	  object.secondBreak = 0
-  }
-  
-  if (thirdBreak.checked) {
-		object.thirdBreak = parseInt(thirdBreak.value, 10)  
-  } else {
-  	  object.thirdBreak = 0
-  }
-  
-  object.workedHours = (object.endTime - object.startTime - object.firstBreak - object.secondBreak - object.thirdBreak) / 60
- 
-  console.log(object)
+  TIME_ENTRY.startTime = parseInt(startTimeValue[0], 10) * 60 + parseInt(startTimeValue[1], 10)
+  TIME_ENTRY.endTime = parseInt(endTimeValue[0], 10) * 60 + parseInt(endTimeValue[1], 10)
+  TIME_ENTRY.workedHours = (TIME_ENTRY.endTime - TIME_ENTRY.startTime) / 60
+
+  console.log(TIME_ENTRY)
+
 })
