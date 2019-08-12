9761d0c519418aaf98de8196bfbbd0f462da42b9
jajan20 <jamie.jansen2@hva.nl>
2019-08-12 17:28:49 +0200

    added pre-defined breaks.

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ let startMinutesInput = document.querySelector('#start-minutes')
 
 let endHoursInput = document.querySelector('#end-hours')
 let endMinutesInput = document.querySelector('#end-minutes')
-// let firstBreak = document.querySelector('#first-break')
-// let secondBreak = document.querySelector('#second-break')
-// let thirdBreak = document.querySelector('#third-break')
+let firstBreak = document.querySelector('#first-break')
+let secondBreak = document.querySelector('#second-break')
+let thirdBreak = document.querySelector('#third-break')
 
 submitBtn.addEventListener('click', (event) => {
   event.preventDefault()
@@ -60,24 +60,27 @@ submitBtn.addEventListener('click', (event) => {
 
   TIME_ENTRY.endTime = totalEndHoursInMinutes + totalEndMinutes
 
-  TIME_ENTRY.workedHours = Math.round((TIME_ENTRY.endTime - TIME_ENTRY.startTime) / 60 * 100) / 100
+  if (firstBreak.checked) {
+  	TIME_ENTRY.firstBreak = parseInt(firstBreak.value, 10)  
+  } else {
+    TIME_ENTRY.firstBreak = 0
+  }
+
+  if (secondBreak.checked) {
+  	TIME_ENTRY.secondBreak = parseInt(secondBreak.value, 10)  
+  } else {
+    TIME_ENTRY.secondBreak = 0
+  }
+
+  if (thirdBreak.checked) {
+  	TIME_ENTRY.thirdBreak = parseInt(thirdBreak.value, 10)  
+  } else {
+    TIME_ENTRY.thirdBreak = 0
+  }
+
+  
+  TIME_ENTRY.workedHours = Math.round((TIME_ENTRY.endTime - TIME_ENTRY.startTime - TIME_ENTRY.firstBreak - TIME_ENTRY.secondBreak - TIME_ENTRY.thirdBreak) / 60 * 100) / 100
   console.log(TIME_ENTRY)
+  
 
-  // if (firstBreak.checked) {
-  // 	object.firstBreak = parseInt(firstBreak.value, 10)  
-  // } else {
-  // 	  object.firstBreak = 0
-  // }
-
-  // if (secondBreak.checked) {
-  // 	object.secondBreak = parseInt(secondBreak.value, 10)  
-  // } else {
-  // 	  object.secondBreak = 0
-  // }
-
-  // if (thirdBreak.checked) {
-  // 	object.thirdBreak = parseInt(thirdBreak.value, 10)  
-  // } else {
-  // 	  object.thirdBreak = 0
-  // }
 })
